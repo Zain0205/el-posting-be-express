565049ad28c284fcca416da84a62e6fe86aaf757
Fahmi Zain <fahmizain000@gmail.com>
2024-10-04 13:45:36 +0700

    make create post feature and get post

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,5 +1,22 @@
-import express from 'express'
+import express from "express";
+import pool from "../models/db.js";
 
-const router = express.Router()
+const router = express.Router();
 
-export default router
+router.post("/create", (req, res) => {
+  const { id, img_url, content } = req.body;
+
+   pool.query("INSERT INTO posts (id, img_url, content) VALUES (?, ?, ?)", [id, img_url, content], (err, result) => {
+    if (err) res.status(400).json({ message: "Error" });
+    res.status(201).json({ message: "Post created" });
+  });
+});
+
+router.get("/feed", (req, res) => {
+  pool.query(`SELECT * FROM posting`, (err, result) => {
+    if (err) res.status(400).json({ message: "Error" });
+    res.status(200).send(result);
+  });
+});
+
+export default router;
