4f0aba81cab1b30086ec7ea163f4bcf3aad9b791
Fahmi Zain <fahmizain000@gmail.com>
2024-10-04 13:44:36 +0700

    removw promise

--- models/db.js
+++ models/db.js
@@ -8,13 +8,7 @@ const pool = createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-}).promise();
+});
 
-export const userRegistration = async (username, password, email) => {
-  const [rows] = await pool.query("INSERT INTO users (username, password, email) VALUES (?, ?, ?)", [username, password, email]);
-
-
-  return rows;
-};
 
 export default pool; 
