03ecfb4ab867ce49e52a6d543d2a4a03e64439ad
Fahmi Zain <fahmizain000@gmail.com>
2024-10-08 19:27:22 +0700

    create get feed for spesific user and get post detail feature

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.post("/create", (req, res) => {
   const { user_id, img_url, content } = req.body;
 
-   pool.query("INSERT INTO posting (id, img_url, content) VALUES (?, ?, ?)", [user_id, img_url, content], (err, result) => {
+  pool.query("INSERT INTO posting (id, img_url, content) VALUES (?, ?, ?)", [user_id, img_url, content], (err, result) => {
     if (err) res.status(400).json({ message: "Error" });
     res.status(201).json({ message: "Post created" });
   });
@@ -19,6 +19,28 @@ router.get("/feed", (req, res) => {
   });
 });
 
+router.get("/feed/:id", (req, res) => {
+  const id = req.params.id;
+
+  pool.query(`SELECT * FROM posting WHERE user_id = ?`, [id], (err, result) => {
+    if (err) res.status(400).json({ message: "Error" });
+    res.status(200).send(result);
+  });
+});
+
+router.get("/feed/detail/:id", (req, res) => {
+  const id = req.params.id;
+
+  pool.query(`
+    SELECT p.id, p.content, p.img_url, p.created_at, p.like_count, u.username 
+    FROM posting as p 
+    JOIN users u on u.id = p.user_id 
+    WHERE p.id = ?`, [id], (err, result) => {
+      if (err) res.status(400).json({ message: "Error" });
+      res.status(200).send(result);
+    });
+});
+
 router.delete("/delete/:id", (req, res) => {
   const id = req.params.id;
 
@@ -26,6 +48,6 @@ router.delete("/delete/:id", (req, res) => {
     if (err) res.status(400).json({ message: "Error" });
     res.status(200).json({ message: "Post deleted" });
   });
-})
+});
 
 export default router;
