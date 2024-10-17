7d139481262431f32bf9304e8f2451f0008fe136
Fahmi Zain <fahmizain000@gmail.com>
2024-10-17 07:38:09 +0700

    create update comment router

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -6,16 +6,20 @@ const router = express.Router();
 router.get("/comment:id", (req, res) => {
   const id = req.params.id;
 
-  pool.query(`
+  pool.query(
+    `
     SELECT c.id, p.id, c.content, u.username, c.created_at, u.img_url
     FROM comments as c
     JOIN posting p on p.id = c.post_id
     JOIN users u on u.id = p.user_id
     WHERE c.post_id = ?;
-`, [id], (err, result) => {
-    if (err) res.status(400).json({ message: "Error" });
-    res.status(200).send(result);
-  });
+`,
+    [id],
+    (err, result) => {
+      if (err) res.status(400).json({ message: "Error" });
+      res.status(200).send(result);
+    }
+  );
 });
 
 router.delete("/delete/:id", (req, res) => {
@@ -24,8 +28,18 @@ router.delete("/delete/:id", (req, res) => {
   pool.query("DELETE FROM comments WHERE id = ?", [id], (err, result) => {
     if (err) res.status(400).json({ message: "Error" });
     res.status(200).json({ message: "Comment deleted" });
-  })
-})
+  });
+});
+
+router.patch("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const { content } = req.body;
+
+  pool.query("UPDATE comments SET content = ? WHERE id = ?", [content, id], (err, result) => {
+    if (err) res.status(400).json({ message: "Error" });
+    res.status(200).json({ message: "Comment updated" });
+  });
+});
 
 router.post("create", (req, res) => {
   const { user_id, post_id, content } = req.body;
@@ -34,9 +48,6 @@ router.post("create", (req, res) => {
     if (err) res.status(400).json({ message: "Error" });
     res.status(201).json({ message: "Comment created" });
   });
-
-})
-
+});
 
 export default router;
-
