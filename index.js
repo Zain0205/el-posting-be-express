5357e6cb173f838f6881ae52e82dd0ead0fe4512
Fahmi Zain <fahmizain000@gmail.com>
2024-10-18 13:41:46 +0700

    use cors

--- index.js
+++ index.js
@@ -5,18 +5,23 @@ import authRoutes from "./routes/authRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/postRoutes.js";
 import likeRoutes from "./routes/likeRoutes.js";
+import followRoutes from "./routes/followRoutes.js";
+import cors from "cors";
 
 dotenv.config();
+
 
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/like", likeRoutes);
+app.use("/api/folloe", followRoutes);
 
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
