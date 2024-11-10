28b487fb20d5eaddd3a7136a815f4ef170ecc1d2
Fahmi Zain <fahmizain000@gmail.com>
2024-11-10 17:42:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,30 @@ import postRoutes from "./routes/postRoutes.js";
 import commentRoutes from "./routes/postRoutes.js";
 import likeRoutes from "./routes/likeRoutes.js";
 import followRoutes from "./routes/followRoutes.js";
+import profileRoutes from "./routes/profileRoutes.js";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
-
 
 const app = express();
 const server = createServer(app);
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173', // sesuaikan dengan alamat frontend
+  credentials: true
+}));
 app.use(express.json());
-app.use('/uploads', express.static('uploads'));
+app.use(cookieParser());
 
-
+app.use("/uploads", express.static("uploads"));
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/like", likeRoutes);
 app.use("/api/follow", followRoutes);
+app.use("/api/profile", profileRoutes);
 
 server.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
