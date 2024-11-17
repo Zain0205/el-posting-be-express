a2a625873f5bda3b43224d10e105af070502ce5f
Fahmi Zain <fahmizain000@gmail.com>
2024-11-17 15:48:32 +0700

    Update profileRoutes.js

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,15 +1,48 @@
 import express from "express";
 import authMiddleware from "../middleware/authMiddleware.js";
 import pool from "../models/db.js";
+import multer from "multer";
+import path from "path";
 
 const router = express.Router();
 
-router.get("/user", authMiddleware, (req, res) => {
-  const id = req.user.id;
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+const upload = multer({ storage });
+
+router.get("/user/:id", (req, res) => {
+  const id = req.params.id;
 
   pool.query(`SELECT * FROM users WHERE id = ?`, [id], (err, result) => {
     if (err) res.status(400).json({ message: "Error" });
     res.status(200).send(result[0]);
+  });
+});
+
+router.get("/recomend", authMiddleware, (req, res) => {
+  const id = req.user.id;
+
+  pool.query(`SELECT * FROM users WHERE id != ?`, [id], (err, result) => {
+    if (err) res.status(400).json({ message: "Error" });
+    res.status(200).send(result);
+  });
+});
+
+router.patch("/edit", upload.single("img_url"), (req, res) => {
+  const { username, bio } = req.body;
+  const id = req.user.id;
+  const img_url = req.file ? `/uploads/${req.file.filename}` : null;
+
+  pool.query("UPDATE users SET username = ?, bio = ?, img_url = ? WHERE id = ?", [username, bio, img_url, background_img, id], (err, result) => {
+    if (err) return res.status(400).json({ message: "Error" });
+    res.status(200).json({ message: "Updated" });
   });
 });
 
