3c3a67e689e405a662f5a0a0f35fdf74c27a9c89
Fahmi Zain <fahmizain000@gmail.com>
2024-12-08 19:33:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ import commentRoutes from "./routes/commentRoutes.js";
 import likeRoutes from "./routes/likeRoutes.js";
 import followRoutes from "./routes/followRoutes.js";
 import profileRoutes from "./routes/profileRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import { Server } from "socket.io";
@@ -37,32 +38,70 @@ app.use("/api/comment", commentRoutes);
 app.use("/api/like", likeRoutes);
 app.use("/api/follow", followRoutes);
 app.use("/api/profile", profileRoutes);
+app.use("/api/chat", chatRoutes);
 
 const onlineUser = new Map();
 
 io.on("connection", (socket) => {
+  console.log("a user connected");
   socket.on("register", (userId) => {
     onlineUser.set(userId, socket.id);
     console.log(`User ${userId} registered with socket id ${socket.id}`);
   });
 
-  socket.on("sendMessage", ({ senderId, receiverId, content }) => {
-    pool.query(`INSERT INTO messages (sender_id, receiver_id, content) VALUES (?, ?, ?)`, [senderId, receiverId, content], (err, result) => {
+  socket.on("sendMessage", ({ sender_id, receiver_id, content }) => {
+    pool.query(`INSERT INTO chats (sender_id, receiver_id, content) VALUES (?, ?, ?)`, [sender_id, receiver_id, content], (err, result) => {
       if (err) {
         console.error(err);
         return;
       }
 
-      const selectedOnlineUser = onlineUser.get(receiverId);
+      pool.query(
+        `
+    SELECT
+    c.id,
+    c.sender_id,
+    sender.username AS sender_username,
+    sender.img_url AS sender_avatar,
+    c.receiver_id,
+    receiver.username AS receiver_username,
+    receiver.img_url AS receiver_avatar,
+    c.content,
+    c.created_at
+FROM chats c
+         JOIN users AS sender ON c.sender_id = sender.id
+         JOIN users AS receiver ON c.receiver_id = receiver.id
+WHERE
+    (c.sender_id = ? AND c.receiver_id = ?) OR
+    (c.sender_id = ? AND c.receiver_id = ?)
+ORDER BY c.created_at DESC
+`,
+        [sender_id, receiver_id, receiver_id, sender_id],
+        (err, result) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
 
-      if (selectedOnlineUser) {
-        io.to(selectedOnlineUser).emit("newMessage", {
-          id: result.insertId,
-          sender_id: senderId,
-          receiver_id: receiverId,
-          content,
-        });
-      }
+          const selectedOnlineUser = onlineUser.get(receiver_id);
+
+          const sender = result[0];
+
+          if (selectedOnlineUser) {
+            io.to(selectedOnlineUser).emit("newMessage", sender);
+          }
+        }
+      );
+
+      // const selectedOnlineUser = onlineUser.get(receiver_id);
+
+      // if (selectedOnlineUser) {
+      //   io.to(selectedOnlineUser).emit("newMessage", {
+      //     sender_id: sender_id,
+      //     receiver_id: receiver_id,
+      //     content,
+      //   });
+      // }
     });
   });
 
